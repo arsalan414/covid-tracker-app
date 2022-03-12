57e908e1169ddd8dacf78cb63b477e27c87853c5
arsalan414 <arsalanbaba414@yahoo.com>
2022-03-12 12:58:51 +0500

    integrated chart js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import Covid from './components/Covid'
+import Covid from './components/Covid';
 
 function App() {
   return (
--- src/components/Covid.js
+++ src/components/Covid.js
@@ -1,20 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+
+
+// chartsjs
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+// ends here
 
 const Div = styled(Paper)(() => ({
   margin: "20px",
   padding: "20px",
 }));
 
-export default function Covid() {
-  const [covidApi, setCovidApi] = useState([]);
 
-  useEffect(() => {
-    getCovid();
-  }, []);
+// export const CountryData = createContext({selectedCountry})
+
+export default function Covid() {
 
   const getCovid = async () => {
     const response = await fetch(
@@ -22,12 +44,58 @@ export default function Covid() {
     );
     const data = await response.json();
     setCovidApi(data);
-    console.log("data api: ", data);
   };
-  console.log("covid api: ", covidApi[0]);
-  let [selectedCountry, setSelectedCountry] = useState({});
 
-  console.log("selected: ", selectedCountry);
+  const [covidApi, setCovidApi] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState({});
+
+  useEffect(() => {
+    getCovid();
+  }, []);
+  
+
+  // charts data
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['All time data'];
+  console.log("cuntry data: ",selectedCountry)
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Cases',
+        data:[selectedCountry.cases.toString()], 
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        label: 'Reacovered',
+        data: [selectedCountry.recovered.toString()],
+        backgroundColor: 'rgba(206, 252, 168, 0.747)',
+      },
+      {
+        label: 'Deaths',
+        data: [selectedCountry.deaths.toString()],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+    
+    
+  };
+  
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <div className="country-container">
@@ -48,18 +116,14 @@ export default function Covid() {
           <Div className="confirmed_cases covid-data">
             <h1>CONFIRMED CASES</h1>
             <h3>
-              {!selectedCountry
-                ? "loading..."
-                : selectedCountry && selectedCountry.cases}
+              {!selectedCountry ? "loading..." : selectedCountry && selectedCountry.cases}
             </h3>
 
             <div className="todays_conf_cases">
               <h3>
                 last 24 hours:{" "}
                 <span>
-                  {!selectedCountry
-                    ? "loading..."
-                    : selectedCountry && selectedCountry.todayCases}
+                  {!selectedCountry ? "loading...": selectedCountry && selectedCountry.todayCases}
                 </span>
               </h3>
             </div>
@@ -70,17 +134,13 @@ export default function Covid() {
             <h1>DEATHS</h1>
             {/* <p>{covidApi[0]["deaths"]}</p> */}
             <h3>
-              {!selectedCountry
-                ? "loading..."
-                : selectedCountry && selectedCountry.deaths}
+              {!selectedCountry ? "loading...": selectedCountry && selectedCountry.deaths}
             </h3>
             <div className="todays_deaths">
               <h3>
                 last 24 hours:{" "}
                 <span>
-                  {!selectedCountry
-                    ? "loading..."
-                    : selectedCountry && selectedCountry.todayDeaths}
+                  {!selectedCountry ? "loading...": selectedCountry && selectedCountry.todayDeaths}
                 </span>
               </h3>
             </div>
@@ -91,65 +151,20 @@ export default function Covid() {
             <h1>RECOVERED</h1>
             {/* <p>{covidApi[0]["recovered"]}</p> */}
             <h3>
-              {!selectedCountry
-                ? "loading..."
-                : selectedCountry && selectedCountry.recovered}
+              {!selectedCountry? "loading...": selectedCountry && selectedCountry.recovered}
             </h3>
             <div className="todays_recovered">
               <h3>
                 last 24 hours:{" "}
                 <span>
-                  {!selectedCountry
-                    ? "loading..."
-                    : selectedCountry && selectedCountry.todayRecovered}
-                </span>
-              </h3>
-            </div>
-          </Div>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Div className="tests covid-data">
-            <h1>TOTAL TESTS</h1>
-            {/* <p>{covidApi[0]["recovered"]}</p> */}
-            <h3>
-              {!selectedCountry
-                ? "loading..."
-                : selectedCountry && selectedCountry.tests}
-            </h3>
-            <div className="todays_tests">
-              <h3>
-                last 24 hours:{" "}
-                <span>
-                  {!selectedCountry
-                    ? "loading..."
-                    : selectedCountry && selectedCountry.todayCases}
-                </span>
-              </h3>
-            </div>
-          </Div>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Div className="critical_cases covid-data">
-            <h1>CRITICAL CASES</h1>
-            {/* <p>{covidApi && covidApi[0].recovered}</p> */}
-            <h3>
-              {!selectedCountry
-                ? "loading..."
-                : selectedCountry && selectedCountry.critical}
-            </h3>
-            <div className="todays_critical_cases">
-              <h3>
-                last 24 hours:{" "}
-                <span>
-                  {!selectedCountry
-                    ? "loading..."
-                    : selectedCountry && selectedCountry.critical}
+                  {!selectedCountry? "loading...": selectedCountry && selectedCountry.todayRecovered}
                 </span>
               </h3>
             </div>
           </Div>
         </Grid>
       </Grid>
+      <Bar className='Barchart' options={options} data={data} />;
     </Box>
   );
 }
